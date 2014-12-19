9005246962d18d9572dfc8972e6fb44a45e442fe
dexterneutron <felixdlugo@gmail.com>
2014-12-19 10:10:46 -0430

    Implementado cifrado DES

--- app/src/main/java/com/example/jascaniojah/libraries/Encrypter.java
+++ app/src/main/java/com/example/jascaniojah/libraries/Encrypter.java
@@ -1,17 +1,24 @@
 package com.example.jascaniojah.libraries;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+
 public class Encrypter {
 
     public static void main(String [] args)
     {
-        SecurityFunctions sf=new SecurityFunctions("0e2ec11cdf82fa49b5c35dfd9d6a654923ee36db","72355628");
-        String text="123456";
+     //   SecurityFunctions sf=new SecurityFunctions("0e2ec11cdf82fa49b5c35dfd9d6a654923ee36db","72355628");
+        SecurityFunctions sf=new SecurityFunctions("4445BBBBBBBBBBBBBBBB");
+        String text="12345678";
+        String formatedText=SecurityFunctions.formatString(text);
         String encoded=sf.encrypt(text);
-        String decoded=sf.decrypt(encoded);
         System.out.println("Original "+text);
+        System.out.println("Formated "+formatedText);
         System.out.println("Encoded "+encoded);
-        System.out.println("Decoded "+decoded);
+
     }
+
+
 
 }
 
--- app/src/main/java/com/example/jascaniojah/libraries/SecurityFunctions.java
+++ app/src/main/java/com/example/jascaniojah/libraries/SecurityFunctions.java
@@ -2,6 +2,8 @@ package com.example.jascaniojah.libraries;
 
 import org.apache.commons.codec.binary.Base64;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
@@ -9,6 +11,7 @@ import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
@@ -19,41 +22,40 @@ public class SecurityFunctions {
     public SecretKey key;
     public IvParameterSpec iv;
 
-    public SecurityFunctions(String keyString, String ivString) {
+    public SecurityFunctions(String keyString) {
         try {
-            final MessageDigest md = MessageDigest.getInstance("md5");
-            final byte[] digestOfPassword = md.digest(Base64.decodeBase64(keyString.getBytes("utf-8")));
+            final byte[] digestOfPassword=hexStringToByteArray(keyString);
             final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
             for (int j = 0, k = 16; j < 8;) {
                 keyBytes[k++] = keyBytes[j++];
             }
 
-            keySpec = new DESedeKeySpec(keyBytes);
+            keySpec = new DESKeySpec(keyBytes);
 
-            key = SecretKeyFactory.getInstance("DESede").generateSecret(keySpec);
-
-            iv = new IvParameterSpec(ivString.getBytes());
+            key = SecretKeyFactory.getInstance("DES").generateSecret(keySpec);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
     public  String encrypt(String value) {
+
         try {
-            Cipher ecipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-            ecipher.init(Cipher.ENCRYPT_MODE, key, iv);
+            Cipher ecipher = Cipher.getInstance("DES/ECB/NoPadding");
+            ecipher.init(Cipher.ENCRYPT_MODE, key);
 
             if(value==null){
                 return null;}
-
+            String formatedValue=formatString(value);
             // Encode the string into bytes using utf-8
-            byte[] utf8 = value.getBytes("UTF8");
 
-            // Encrypt
-            byte[] enc = ecipher.doFinal(utf8);
+            byte[] hex = hexStringToByteArray(formatedValue);
+
+            byte[] enc = ecipher.doFinal(hex);
 
             // Encode bytes to base64 to get a string
-            return new String(Base64.encodeBase64(enc),"UTF-8");
+           // return new String(Base64.encodeBase64(enc),"HEX");
+            return byteArrayToHex(enc).toUpperCase();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -64,7 +66,7 @@ public class SecurityFunctions {
     public String decrypt(String value) {
         try {
             Cipher dcipher = Cipher.getInstance("DESede/CBC/PKCS5Padding","SunJCE");
-            dcipher.init(Cipher.DECRYPT_MODE, key, iv);
+            dcipher.init(Cipher.DECRYPT_MODE, key,iv);
 
             if(value==null)
                 return null;
@@ -82,6 +84,47 @@ public class SecurityFunctions {
         }
         return null;
     }
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 
+    public static String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for(byte b: a)
+            sb.append(String.format("%02x", b & 0xff));
+        return sb.toString();
+    }
+    public static String formatString(String s)
+    {
+        String parsedString=s;
+        int  stringLeng=s.length();
+
+        if (stringLeng==6)
+        {   parsedString=toHex(parsedString,12);
+            parsedString="2020"+parsedString;
+        }
+        else if(stringLeng==7) {
+            parsedString=toHex(parsedString,14);
+            parsedString = "20"+parsedString;
+        }
+        else {
+            parsedString=toHex(parsedString,16);}
+        return parsedString;
+    }
+
+    public static  String toHex(String s,int number ) {
+        try {
+            return String.format("%0"+number+"x", new BigInteger(1, s.getBytes("UTF-8")));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
 }
 
